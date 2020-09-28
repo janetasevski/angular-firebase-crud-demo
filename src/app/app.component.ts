26b230ebb772e5854c5d27317c2b15a5cff592c4
Jane Tasevski <janetasevski@MacBook-Pro.local>
2020-09-28 18:21:10 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { IEmployee, FireBaseService } from './services/fire-base.service';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'firebase-crud-demo';
+export class AppComponent implements OnInit {
+  public form: FormGroup;
+
+  public employeeList: IEmployee[] = [];
+  public employeeDetails: IEmployee;
+  
+  constructor(
+    private fb: FormBuilder,
+    private modalService: NgbModal,
+    private fireBaseService: FireBaseService
+  ) {}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  getEmployees(): void {
+    this.fireBaseService.getEmployees()
+      .subscribe((res) => {
+        this.employeeList = res.map((employee) => {
+          return {
+            ...employee.payload.doc.data() as {},
+            id: employee.payload.doc.id
+          } as IEmployee;
+        });
+      });
+  }
+
+  openModal(content: TemplateRef<any>, employeeId: string): void {
+    this.employeeDetails = this.employeeList
+      .find((employee: IEmployee) => employee.id === employeeId);
+    
+    this.formInit(this.employeeDetails);
+    this.modalService.open(content, {backdrop: 'static', centered: true})
+  }
+
+  formInit(data: IEmployee): void {
+    this.form = this.fb.group({
+      name: [data ? data.name : '', Validators.required],
+      email: [
+        data ? data.email : '', 
+        Validators.compose([
+          Validators.required
+        ])
+      ]
+    })
+  }
+
+  addEmployee(): void {
+    this.fireBaseService.addEmployee(this.form.value).then();
+  }
+
+  updateEmployee(employeeId: string): void {
+    this.fireBaseService.updateEmployee(employeeId, this.form.value).then();
+  }
+
+  deleteEmployee(employeeId: string): void {
+    this.fireBaseService.deleteEmployee(employeeId).then();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
